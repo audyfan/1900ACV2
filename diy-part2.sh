905d45140a8c87574f10f7ae5cb28a03baf87780
audyfan <fanshumin@hotmail.com>
2022-08-31 20:07:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.6.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-git clone https://github.com/Boos4721/packages
-git clone https://github.com/Boos4721/OpenWrt-Packages
